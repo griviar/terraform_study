910d74d6dcb3951da368698b4fd0f508d4be311b
griviar <griviar@gmail.com>
2022-10-25 15:18:17 +0300

    started tf file

--- yc-connect/Network/main.tf
+++ yc-connect/Network/main.tf
@@ -2,6 +2,36 @@
 #Test terraform 
 #--------------
 
-provider "yc" {
-  
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
 }
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+#------------------------
+
+variable "vpc_cidr" {
+  default = [
+    "10.100.1.0/24",
+    "10.100.2.0/24",
+  ]
+}
+
+
+
+#------------------------
+
+resource "yandex_vpc_network" "test_vpc" {
+    name = "test_network"
+}
+
+resource "yandex_vpc_network" "test_network" {
+  count = lenght()
+}
+
